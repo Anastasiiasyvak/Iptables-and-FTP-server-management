209b13a9bc614f38e948c8c232f29cee1e803ae0
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-03-29 16:58:10 +0200

    Create update_iptables.sh

---
+++ update_iptables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+credentials_file="credentials.txt"
+
+if [ ! -f "$credentials_file" ]; then
+    echo "Error: credentials.txt file not found."
+    exit 1
+fi
+
+echo "Enter authorization key for the connected IP address:"
+read -r authorization_key
+
+client_ip="$SOCAT_PEERADDR"
+
+echo "Authorization key: $authorization_key"
+
+if grep -q "$client_ip $authorization_key" "$credentials_file"; then
+    iptables -A INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT
+    echo "Authorized IP address added to allow list for FTP access."
+else
+    iptables -D INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT 2>/dev/null
+    echo "Unauthorized IP address discarded from allow list for FTP access."
+fi

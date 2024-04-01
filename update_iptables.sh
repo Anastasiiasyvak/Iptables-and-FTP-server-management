226bc7db8f4684953b6ba65b364b86605b9e78f2
Anastasiiasyvak <119412566+Anastasiiasyvak@users.noreply.github.com>
2024-04-01 12:02:22 +0300

    Update update_iptables.sh

--- update_iptables.sh
+++ update_iptables.sh
@@ -7,20 +7,17 @@ if [ ! -f "$credentials_file" ]; then
     exit 1
 fi
 
-# Я видаляю попередні правила iptables, щоб уникати дублювання
-iptables -D INPUT -p tcp --dport 21 -j ACCEPT 2>/dev/null
-
 echo "Enter authorization key for the connected IP address:"
-read -r authorization_key
+read authorization_key
 
 client_ip="$SOCAT_PEERADDR"
 
 echo "Authorization key: $authorization_key"
 
-if grep -q "$client_ip $authorization_key" "$credentials_file"; then
-    iptables -A INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT
+if grep -qs "$client_ip $authorization_key" "$credentials_file"; then
+    iptables -I INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT
     echo "Authorized IP address added to allow list for FTP access."
 else
-    iptables -D INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT 2>/dev/null
+    iptables -D INPUT -s "$client_ip" -p tcp --dport 21 -j ACCEPT 
     echo "Unauthorized IP address discarded from allow list for FTP access."
 fi
